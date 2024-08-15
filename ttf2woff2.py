a506e49bc972a0518a306e2eca73e42bb6ea3e55
leopard <937388985@qq.com>
2024-08-15 23:27:12 +0800

    11

---
+++ ttf2woff2.py
@@ -0,0 +1,13 @@
+﻿from fontTools.ttLib import TTFont
+from fontTools.ttLib.woff2 import compress
+ 
+ttf_file_path = 'build/SourceHanSansCN-Normal.otf'
+woff2_file_path = 'build/SourceHanSansCN-Normal.woff2'
+
+# 转换方法1
+# compress(ttf_file_path, woff2_file_path)
+
+# 转换方法2
+f = TTFont(ttf_file_path)
+f.flavor='woff2'
+f.save(woff2_file_path)
